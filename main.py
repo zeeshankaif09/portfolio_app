157ecaf650bc7ae19ddb70ae3d3d7cb08bb1c65a
Zeeshankaif <zeeshankaif09@gmaail.com>
2023-01-08 02:08:11 +0530

    add app discription image and link

--- main.py
+++ main.py
@@ -19,15 +19,21 @@ Below you can find some of the app i have built in python feel free contact me
 """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+       #st.write("[source code](https://")
+        st.write(f"[source code]({row['url']})")
 
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
