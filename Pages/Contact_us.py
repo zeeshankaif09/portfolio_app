3814c55f03606411a6a3435d9d459e5dcda42c70
Zeeshankaif <zeeshankaif09@gmaail.com>
2023-01-09 04:39:00 +0530

    impliment button

--- Pages/Contact_us.py
+++ Pages/Contact_us.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.header("Contact me")
+
+with st.form(key="My_Forms"):
+    user_email = st.text_input("Your Email Address")
+    message = st.text_area("Your Message")
+    button = st.form_submit_button("Sumbit")
+    print(button)
+    if button:
+        print("Thank u for contacting us")
